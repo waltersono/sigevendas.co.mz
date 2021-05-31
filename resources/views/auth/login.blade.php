731da9b7b6cf7570fd52cb230f880c7fc7db418e
Walter Sono <tec.waltersono@gmail.com>
2021-05-31 15:34:37 +0200

    test git remote

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@ Entrar
     @include('partials.errors')
     @include('partials.alerts')
 
-    <h4 class="h5 mb-0 font-weight-bold text-light">SGV</h4>
+    <h4 class="h5 mb-0 font-weight-bold text-light">SIGEVENDAS</h4>
     <h4 class="h5 mb-3 font-weight-bold text-light">Sistema Gestao de Vendas</h4>
 
     <label for="inputEmail" class="sr-only">Email</label>
@@ -22,7 +22,7 @@ Entrar
     <button type="submit" class="btn btn-sm btn-success btn-block" type="submit">Entrar</button>
     @csrf
     <p class="text-center text-light mt-2 mb-0">
-        Copyright © <?php echo date('yy');?> <strong>SGV</strong>. Todos os Direitos Reservados.
+        Copyright © <?php echo date('yy'); ?> <strong>SGV</strong>. Todos os Direitos Reservados.
     </p>
     <p class="text-center text-light mt-0">
         Desenvolvido pela <a href="http://www.google.com" target="_blank" class="text-light">SoftTech,Lda</a>.
